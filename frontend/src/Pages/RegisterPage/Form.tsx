d0152bc1fe7abd3a3bcd2b7dd5107f9f9124f844
Vocyl <platyna155@gmail.com>
2023-12-20 21:33:22 +0100

    Added handleSubmit function and loader to button

--- frontend/src/Pages/RegisterPage/Form.tsx
+++ frontend/src/Pages/RegisterPage/Form.tsx
@@ -1,21 +1,68 @@
 import { useState } from "react";
-
 import NameInput from "../../components/NameInput";
 import PasswordInput from "../../components/PasswordInput";
 import SignUpButton from "./SignUpButton";
 import EmailInput from "../../components/EmailInput";
 import InfoText from "./InfoText";
+import { REGISTER_URL } from "../../constraints";
+
+type NewUser = {
+  email: string;
+  name: string;
+  password: string;
+};
+
+type ErrorResponse = {
+  message: string;
+};
 
 const Form = () => {
   const [email, setEmail] = useState<string>("");
   const [name, setName] = useState<string>("");
-  const [pwd, setPwd] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [fetchStatus, setFetchStatus] = useState<string>("");
+  const handleSubmit = async (
+    password: string,
+    email: string,
+    name: string
+  ) => {
+    try {
+      const data: NewUser = { password, email, name };
+      setFetchStatus("");
+      setIsLoading(true);
+      const response = await fetch(REGISTER_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (!response.ok) {
+        const json = (await response.json()) as ErrorResponse;
+
+        throw new Error(`${json.message}`);
+      }
+
+      setFetchStatus("Signed in succesfully!");
+    } catch (err: Error | unknown) {
+      if (err instanceof Error) {
+        setFetchStatus(err.message);
+        console.log(err.message);
+      } else {
+        console.log(err);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <form
       className="space-y-4   w-[400px] mx-auto  my-auto"
       onSubmit={(e) => {
         e.preventDefault();
+        handleSubmit(password, email, name);
       }}
     >
       <h1 className="text-black text-4xl  font-semibold text-center leading-[44px]">
@@ -26,9 +73,20 @@ const Form = () => {
       </h2>
       <NameInput name={name} setName={setName} />
       <EmailInput email={email} setEmail={setEmail} />
-      <PasswordInput pwd={pwd} setPwd={setPwd} />
+      <PasswordInput pwd={password} setPwd={setPassword} />
       <InfoText />
-      <SignUpButton />
+      <SignUpButton isLoading={isLoading} />
+      {fetchStatus && (
+        <p
+          className={`${
+            fetchStatus === "Signed in succesfully!"
+              ? "text-green-500"
+              : "text-red-500"
+          } pt-1 mx-auto w-fit text-center`}
+        >
+          {fetchStatus}
+        </p>
+      )}
     </form>
   );
 };
--- frontend/src/Pages/RegisterPage/SignUpButton.tsx
+++ frontend/src/Pages/RegisterPage/SignUpButton.tsx
@@ -1,10 +1,20 @@
-const SignUpButton = () => {
+type Props = {
+  isLoading: boolean;
+};
+
+const SignUpButton = ({ isLoading }: Props) => {
   return (
     <button
       type="submit"
-      className="bg-teal-500 text-white text-xl font-semibold uppercase leading-[14.50px]  border border-none rounded-xl w-[400px] h-[55px] flex-shrink-0"
+      className="bg-teal-500 text-white text-xl font-semibold uppercase leading-[14.50px] justify-center flex items-center  border border-none rounded-xl w-[400px] h-[55px] flex-shrink-0"
     >
-      sign up
+      {!isLoading ? (
+        <p>sign up</p>
+      ) : (
+        <div className="grid w-8 h-8 place-items-center">
+          <div className="w-full h-full border-[6px] rounded-full border-cyan-600 border-t-slate-200 animate-spin"></div>
+        </div>
+      )}
     </button>
   );
 };
