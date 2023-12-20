ebc6d0e2169aa5691dafb44b7a63975e0aa4c362
Vocyl <platyna155@gmail.com>
2023-12-20 13:09:17 +0100

    Added routes and started doing register page

---
+++ frontend/src/Pages/RegisterPage/RegisterPage.tsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+
+const RegisterPage = () => {
+  return (
+    <main className="flex w-full h-full">
+      <section className="flex flex-col w-full h-full p-10 bg-zinc-100">
+        <Header />
+      </section>
+      <img src="/public/socialmedia.png" alt="socialmedia" />
+    </main>
+  );
+};
+export default RegisterPage;
