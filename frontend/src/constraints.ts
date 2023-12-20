71466fa2a2dbed128b4da1203bd85557ab1fbc29
DominikKoniarz <dominik.koniarz.development@gmail.com>
2023-12-20 21:16:06 +0100

    Consts fix

--- frontend/src/constraints.ts
+++ frontend/src/constraints.ts
@@ -1,4 +1,5 @@
 const APP_URL = import.meta.env.APP_URL || "http://localhost:3000";
+const API_URL = `${APP_URL}/api`;
 
-export const REGISTER_URL = `${APP_URL}/register`;
-export const LOGIN_URL = `${APP_URL}/login`;
+export const REGISTER_URL = `${API_URL}/register`;
+export const LOGIN_URL = `${API_URL}/login`;
