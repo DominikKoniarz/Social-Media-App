d9e0201a0dbd944711d5eb550b2ec7fc6e774773
DominikKoniarz <dominik.koniarz.development@gmail.com>
2023-12-21 17:13:58 +0100

    Handle login submit - added required fetch config

--- frontend/src/Pages/LoginPage/Form.tsx
+++ frontend/src/Pages/LoginPage/Form.tsx
@@ -6,82 +6,83 @@ import { LOGIN_URL } from "../../constraints";
 import { useNavigate } from "react-router-dom";
 
 type LoginData = {
-  email: string;
-  password: string;
+	email: string;
+	password: string;
 };
 
 type ErrorResponse = {
-  message: string;
+	message: string;
 };
 
 const Form = () => {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [fetchStatus, setFetchStatus] = useState<string>("");
+	const [email, setEmail] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [fetchStatus, setFetchStatus] = useState<string>("");
 
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const handleSubmit = async (email: string, password: string) => {
-    try {
-      const data: LoginData = { email, password };
-      setFetchStatus("");
-      setIsLoading(true);
-      const response = await fetch(LOGIN_URL, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-      if (!response.ok) {
-        const json = (await response.json()) as ErrorResponse;
+	const handleSubmit = async (email: string, password: string) => {
+		try {
+			const data: LoginData = { email, password };
+			setFetchStatus("");
+			setIsLoading(true);
+			const response = await fetch(LOGIN_URL, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				credentials: "include",
+				body: JSON.stringify(data),
+			});
+			if (!response.ok) {
+				const json = (await response.json()) as ErrorResponse;
 
-        throw new Error(`${json.message}`);
-      }
-      navigate("/profilebar");
-      setFetchStatus("Logged in succesfully!");
-    } catch (err: Error | unknown) {
-      if (err instanceof Error) {
-        setFetchStatus(err.message);
-        console.log(err.message);
-      } else {
-        console.log(err);
-      }
-    } finally {
-      setIsLoading(false);
-    }
-  };
+				throw new Error(`${json.message}`);
+			}
+			navigate("/profilebar");
+			setFetchStatus("Logged in succesfully!");
+		} catch (err: Error | unknown) {
+			if (err instanceof Error) {
+				setFetchStatus(err.message);
+				console.log(err.message);
+			} else {
+				console.log(err);
+			}
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
-  return (
-    <form
-      className="space-y-4   w-[400px] mx-auto  my-auto"
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleSubmit(email, password);
-      }}
-    >
-      <h1 className="text-black text-4xl  font-semibold text-center leading-[44px]">
-        Welcome Back
-      </h1>
-      <h2 className="text-black block pb-1 text-lg font-normal text-center  leading-[29.16px]">
-        Log in into your account
-      </h2>
-      <EmailInput email={email} setEmail={setEmail} />
-      <PasswordInput pwd={password} setPwd={setPassword} />
-      <LogInButton isLoading={isLoading} />
-      {fetchStatus && (
-        <p
-          className={`${
-            fetchStatus === "Logged in succesfully!"
-              ? "text-green-500"
-              : "text-red-500"
-          } pt-1 mx-auto w-fit text-center`}
-        >
-          {fetchStatus}
-        </p>
-      )}
-    </form>
-  );
+	return (
+		<form
+			className="space-y-4   w-[400px] mx-auto  my-auto"
+			onSubmit={(e) => {
+				e.preventDefault();
+				handleSubmit(email, password);
+			}}
+		>
+			<h1 className="text-black text-4xl  font-semibold text-center leading-[44px]">
+				Welcome Back
+			</h1>
+			<h2 className="text-black block pb-1 text-lg font-normal text-center  leading-[29.16px]">
+				Log in into your account
+			</h2>
+			<EmailInput email={email} setEmail={setEmail} />
+			<PasswordInput pwd={password} setPwd={setPassword} />
+			<LogInButton isLoading={isLoading} />
+			{fetchStatus && (
+				<p
+					className={`${
+						fetchStatus === "Logged in succesfully!"
+							? "text-green-500"
+							: "text-red-500"
+					} pt-1 mx-auto w-fit text-center`}
+				>
+					{fetchStatus}
+				</p>
+			)}
+		</form>
+	);
 };
 export default Form;
--- frontend/src/Pages/LoginPage/LogInButton.tsx
+++ frontend/src/Pages/LoginPage/LogInButton.tsx
@@ -1,21 +1,21 @@
 type Props = {
-  isLoading: boolean;
+	isLoading: boolean;
 };
 
 const LogInButton = ({ isLoading }: Props) => {
-  return (
-    <button
-      type="submit"
-      className="bg-teal-500 text-white text-xl font-semibold uppercase leading-[14.50px]  border border-none rounded-xl w-[400px] h-[55px] flex-shrink-0"
-    >
-      {!isLoading ? (
-        <p>log in</p>
-      ) : (
-        <div className="grid w-8 h-8 place-items-center">
-          <div className="w-full h-full border-[6px] rounded-full border-cyan-600 border-t-slate-200 animate-spin"></div>
-        </div>
-      )}
-    </button>
-  );
+	return (
+		<button
+			type="submit"
+			className="bg-teal-500 flex justify-center items-center text-white text-xl font-semibold uppercase leading-[14.50px]  border border-none rounded-xl w-[400px] h-[55px] flex-shrink-0"
+		>
+			{!isLoading ? (
+				"log in"
+			) : (
+				<div className="grid w-8 h-8 place-items-center">
+					<div className="w-full h-full border-[6px] rounded-full border-cyan-600 border-t-slate-200 animate-spin"></div>
+				</div>
+			)}
+		</button>
+	);
 };
 export default LogInButton;
