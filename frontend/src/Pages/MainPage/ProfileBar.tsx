7e4958d644ee518c7ba903e6149bbc4afdeda502
Vocyl <platyna155@gmail.com>
2023-12-21 22:22:25 +0100

    Fixed path in profilebar

--- frontend/src/Pages/MainPage/ProfileBar.tsx
+++ frontend/src/Pages/MainPage/ProfileBar.tsx
@@ -1,8 +1,9 @@
+import imgF from "../../assets/imgF.png";
 const ProfileBar = () => {
   return (
     <div className="w-[246px] h-full bg-color2 border border-none rounded-tl-xl flex flex-col rounded-tr-xl">
       <div className="flex items-center justify-center w-full bg-color1 rounded-tr-xl  border border-none rounded-tl-xl h-[75px]">
-        <img className="mix-blend-darken" src="imgF.png" alt="" />
+        <img className="mix-blend-darken" src={imgF} alt="ProfilePicture" />
       </div>
       <span className="flex justify-center pt-2 text-xl font-medium text-white">
         Placeholder
