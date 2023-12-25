326092dafd71af9d453b5bddd61065b902839d7a
Vocyl <platyna155@gmail.com>
2023-12-25 18:05:13 +0100

    Initial profile layout

--- frontend/src/Pages/ProfilePage/ProfilePage.tsx
+++ frontend/src/Pages/ProfilePage/ProfilePage.tsx
@@ -1,4 +1,18 @@
+import ProfileBackground from "@assets/images/ProfileBackground.png";
+import ProfilePicture from "@assets/images/ProfilePicture.png";
+
 const ProfilePage = () => {
-	return <div className="w-full h-full p-0">profile page</div>;
+  return (
+    <div className="w-full h-full px-32">
+      <div className="flex flex-col w-full h-fit">
+        <img src={ProfileBackground} alt="ProfileBackground" />
+        <div className="absolute mt-32 ml-12">
+          <img src={ProfilePicture} alt="ProfilePicture" />
+        </div>
+        <div className="flex items-center justify-end "></div>
+      </div>
+      <div className="w-full h-fit"></div>
+    </div>
+  );
 };
 export default ProfilePage;
