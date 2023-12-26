38c06a9ecf964ca292043511e4edfb5ea13cd1b0
Vocyl <platyna155@gmail.com>
2023-12-26 20:42:46 +0100

    Added cancel button functionality

--- frontend/src/Pages/ProfilePage/ProfileEdit/CancelButton.tsx
+++ frontend/src/Pages/ProfilePage/ProfileEdit/CancelButton.tsx
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 const CancelButton = () => {
+  const navigate = useNavigate();
+  const handleCancel = () => {
+    navigate("/profile");
+  };
+
   return (
     <button
-      type="submit"
+      onClick={handleCancel}
+      type="button"
       className="text-teal-500 text-xl font-semibold  bg-white uppercase leading-[14.50px] border-2 border-teal-500 rounded-md py-4 px-16"
     >
       cancel
