1a5c02a1f4b9655b0c1c14a5fb086aac3c416547
Vocyl <platyna155@gmail.com>
2023-12-26 18:47:18 +0100

    Prototype inputs

--- frontend/src/Pages/ProfilePage/ProfileEdit/ProfileEdit.tsx
+++ frontend/src/Pages/ProfilePage/ProfileEdit/ProfileEdit.tsx
@@ -1,8 +1,13 @@
 import ProfileBackground from "@assets/images/ProfileBackground.png";
 import ProfilePicture from "@assets/images/ProfilePicture.png";
+import { useState } from "react";
 import { FaPen } from "react-icons/fa6";
 
 const ProfileEdit = () => {
+  const [userName, setUserName] = useState<string>("");
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+
   return (
     <div className="w-full h-full px-6">
       <div className="relative flex flex-col w-full h-fit">
@@ -27,33 +32,63 @@ const ProfileEdit = () => {
           </div>
         </button>
       </div>
-      <section className="w-full px-10 pt-24 pb-10 bg-white h-fit ">
-        <div className="flex gap-16">
-          <div className="flex flex-col w-full">
+      <form className="w-full px-10 pt-24 pb-10 bg-white h-fit ">
+        <div className="flex w-full gap-12">
+          <div className="flex flex-col w-[40%]">
             <p className="py-1">User Name</p>
+            <label
+              htmlFor="UserName-input"
+              className=" hidden absolute left-[-9999px]"
+            >
+              UserName
+            </label>
             <input
               className="w-full p-4 border rounded bg-neutral-100 border-neutral-200"
               type="text"
+              placeholder="User Name"
+              id="UserName-input"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
             />
           </div>
-          {/* <div className="flex gap-8 "> */}
-          <div className="flex flex-col w-full">
-            <p className="py-1">First Name</p>
-            <input
-              className="w-full p-4 border rounded bg-neutral-100 border-neutral-200"
-              type="text"
-            />
-            {/* </div> */}
-            {/* <div className="flex w-[50%] flex-col">
-              <p>Last Name</p>
+          <div className="flex gap-8  w-[60%] ">
+            <div className="flex flex-col w-full">
+              <p className="py-1">First Name</p>
+              <label
+                htmlFor="FirstName-input"
+                className=" hidden absolute left-[-9999px]"
+              >
+                First Name
+              </label>
               <input
                 className="w-full p-4 border rounded bg-neutral-100 border-neutral-200"
                 type="text"
+                placeholder="First Name"
+                id="FirstName-input"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
               />
-            </div> */}
+            </div>
+            <div className="flex flex-col w-full">
+              <p className="py-1">Last Name</p>
+              <label
+                htmlFor="LastName-input"
+                className=" hidden absolute left-[-9999px]"
+              >
+                LastName
+              </label>
+              <input
+                className="w-full p-4 border rounded bg-neutral-100 border-neutral-200"
+                type="text"
+                placeholder="Last Name"
+                id="LastName-input"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </div>
           </div>
         </div>
-      </section>
+      </form>
     </div>
   );
 };
