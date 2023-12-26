5f77e00a1c9b1f277a98fadbc3d5790db560bfdf
Vocyl <platyna155@gmail.com>
2023-12-26 19:01:46 +0100

    Added prototype text area

--- frontend/src/Pages/ProfilePage/ProfileEdit/ProfileEdit.tsx
+++ frontend/src/Pages/ProfilePage/ProfileEdit/ProfileEdit.tsx
@@ -12,13 +12,26 @@ const ProfileEdit = () => {
   return (
     <div className="w-full h-full px-6">
       <ProfileEditHeader />
-      <form className="w-full px-10 pt-24 pb-10 bg-white h-fit ">
+      <form className="flex flex-col w-full px-10 pt-24 pb-10 space-y-4 bg-white h-fit ">
         <div className="flex w-full gap-12">
           <UserNameInput userName={userName} setUserName={setUserName} />
-          <div className="flex gap-8  w-[60%] ">
+          <div className="flex gap-8 w-[60%]">
             <FirstNameInput firstName={firstName} setFirstName={setFirstName} />
             <LastNameInput lastName={lastName} setLastName={setLastName} />
           </div>
+        </div>
+        <div className="">
+          <p className="py-1">About me</p>
+          <label
+            htmlFor="UserName-input"
+            className=" hidden absolute left-[-9999px]"
+          >
+            About me
+          </label>
+          <textarea
+            id="About me"
+            className="w-full p-4 border rounded bg-neutral-100 border-neutral-200"
+          />
         </div>
       </form>
     </div>
