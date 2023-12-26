c8e519dfea4017db603fb6c75f8053d0d6561b36
Vocyl <platyna155@gmail.com>
2023-12-26 21:36:05 +0100

    Added loader style

--- frontend/src/Pages/ProfilePage/ProfileEdit/Loader.tsx
+++ frontend/src/Pages/ProfilePage/ProfileEdit/Loader.tsx
@@ -1,3 +1,11 @@
 export default function Loader() {
-	return <div>Loading...</div>;
+  return (
+    <div className="loader">
+      <span className="dot"></span>
+      <span className="dot"></span>
+      <span className="dot"></span>
+      <span className="dot"></span>
+      <span className="dot"></span>
+    </div>
+  );
 }
