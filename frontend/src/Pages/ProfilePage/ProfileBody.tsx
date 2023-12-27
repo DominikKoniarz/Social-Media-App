badb702b44085524a70a8d5513eb8e00d8541783
Vocyl <platyna155@gmail.com>
2023-12-27 22:49:11 +0100

    Added userdata

--- frontend/src/Pages/ProfilePage/ProfileBody.tsx
+++ frontend/src/Pages/ProfilePage/ProfileBody.tsx
@@ -1,28 +1,47 @@
+import useSocketContext from "hooks/useSocketContext";
+import Loader from "./ProfileEdit/Loader";
+
 const ProfileBody = () => {
+  const { userData } = useSocketContext();
+
   return (
     <section className="w-full px-10 pt-3 pb-4 bg-white h-fit">
-      <article className="flex items-center gap-4 ">
-        <p className="text-xl font-medium text-zinc-950 font-family1">
-          Placeholder Name
-        </p>
-        <p className="text-base font-light lowercase text-slate-800 font-family1">
-          @PlaceholderAlias
-        </p>
-      </article>
-      <p className="text-base font-normal text-teal-500 font-family1">online</p>
-      <section className="flex flex-col py-4">
-        <p className="text-zinc-950 pb-2 text-[19px] font-medium font-family1">
-          About Me
-        </p>
-        <article className="text-base font-light leading-normal text-black font-family1">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged.
-        </article>
-      </section>
+      {userData ? (
+        <>
+          <article className="flex items-center gap-4 ">
+            {userData.firstname && userData.lastname && (
+              <p className="text-xl font-medium text-zinc-950 font-family1">
+                {`${userData.firstname} ${userData.lastname}`}
+              </p>
+            )}
+
+            <p className="text-base font-light lowercase text-slate-800 font-family1">
+              @{userData.username}
+            </p>
+          </article>
+          <p className="text-base font-normal text-teal-500 font-family1">
+            online
+          </p>
+          <section className="flex flex-col py-4">
+            <p className="text-zinc-950 pb-2 text-[19px] font-medium font-family1">
+              About Me
+            </p>
+            {!userData.bio ? (
+              <article className="text-base font-light leading-normal text-black opacity-50 font-family1">
+                No bio to display
+              </article>
+            ) : (
+              <article className="text-base font-light leading-normal text-black font-family1">
+                {userData.bio?.replace("\n", "<br>")}
+              </article>
+            )}
+          </section>
+        </>
+      ) : (
+        <div className="pb-16">
+          <Loader />
+        </div>
+      )}
     </section>
   );
 };
