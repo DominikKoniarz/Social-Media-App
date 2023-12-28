4b35ac8bed847fffb934c2e376ad92c80fc56c8b
Vocyl <platyna155@gmail.com>
2023-12-28 19:18:19 +0100

    Modified NavBar and UserPosts

--- frontend/src/Pages/ProfilePage/UserPost.tsx
+++ frontend/src/Pages/ProfilePage/UserPost.tsx
@@ -1,51 +1,70 @@
 import imgF from "@assets/images/imgF.png";
 import PlaceholderImage from "@assets/images/PlaceholderImage.png";
+import useSocketContext from "hooks/useSocketContext";
 import {
   FaRegTrashCan,
   FaRegHeart,
   FaRegMessage,
   FaRegBookmark,
 } from "react-icons/fa6";
+import Loader from "./ProfileEdit/Loader";
 
 const UserPost = () => {
+  const { userData } = useSocketContext();
   return (
     <li className="flex flex-col w-full px-4 py-4 bg-white h-fit">
-      <div className="flex items-center gap-1">
-        <div className="grid w-12 h-12 p-1 border border-black rounded-full place-items-center">
-          <img className="mix-blend-darken" src={imgF} alt="ProfilePicture" />
+      {userData ? (
+        <>
+          <div className="flex items-center gap-1">
+            <div className="grid w-12 h-12 p-1 border border-black rounded-full place-items-center">
+              <img
+                className="mix-blend-darken"
+                src={imgF}
+                alt="ProfilePicture"
+              />
+            </div>
+            {userData.firstname && userData.lastname && (
+              <p className="ml-2 text-lg font-medium text-zinc-950">
+                {`${userData.firstname} ${userData.lastname}`}
+              </p>
+            )}
+            <p className="ml-1 text-base font-light lowercase text-slate-800 font-family1">
+              @{userData.username}
+            </p>
+            <p className="text-base font-light lowercase text-slate-800">
+              - 10min
+            </p>
+            <button className="ml-auto mr-2 text-2xl">
+              <FaRegTrashCan />
+            </button>
+          </div>
+          <div className="flex flex-col ">
+            <p className="py-2">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+            </p>
+            <img
+              className="mix-blend-darken"
+              src={PlaceholderImage}
+              alt="ProfilePicture"
+            />
+          </div>
+          <div className="flex items-center w-full h-full gap-8 py-5 ">
+            <div className="flex items-center gap-2 text-base font-normal lowercase text-slate-600">
+              <FaRegHeart className="text-lg" /> 123
+            </div>
+            <div className="flex items-center gap-2 text-base font-normal lowercase text-slate-600">
+              <FaRegMessage className="text-lg" /> 3123
+            </div>
+            <div className="flex items-center gap-2 text-base font-normal lowercase text-slate-600">
+              <FaRegBookmark className="text-lg" /> 3123
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="pb-16">
+          <Loader />
         </div>
-        <p className="ml-2 text-lg font-medium text-zinc-950">
-          Placeholder Name
-        </p>
-        <p className="ml-1 text-base font-light lowercase text-slate-800 font-family1">
-          @AliasDanegoUżytkownika
-        </p>
-        <p className="text-base font-light lowercase text-slate-800">- 10min</p>
-        <button className="ml-auto mr-2 text-2xl">
-          <FaRegTrashCan />
-        </button>
-      </div>
-      <div className="flex flex-col ">
-        <p className="py-2">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-        </p>
-        <img
-          className="mix-blend-darken"
-          src={PlaceholderImage}
-          alt="ProfilePicture"
-        />
-      </div>
-      <div className="flex items-center w-full h-full gap-8 py-5 ">
-        <div className="flex items-center gap-2 text-base font-normal lowercase text-slate-600">
-          <FaRegHeart className="text-lg" /> 123
-        </div>
-        <div className="flex items-center gap-2 text-base font-normal lowercase text-slate-600">
-          <FaRegMessage className="text-lg" /> 3123
-        </div>
-        <div className="flex items-center gap-2 text-base font-normal lowercase text-slate-600">
-          <FaRegBookmark className="text-lg" /> 3123
-        </div>
-      </div>
+      )}
     </li>
   );
 };
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,22 +1,24 @@
 import imgF from "@assets/images/imgF.png";
 import LinksList from "./LinksList";
 import LogoutButton from "./LogoutButton";
+import useSocketContext from "hooks/useSocketContext";
 
 const NavBar = () => {
-	return (
-		<nav className="w-[246px] shrink-0 h-full bg-color2 border border-none rounded-xl overflow-hidden flex flex-col">
-			<div className="flex items-center justify-center w-full bg-color1 rounded-tr-xl border border-none rounded-tl-xl h-[75px]">
-				<div className="p-1 py-2 border border-white rounded-full ">
-					<img className="mix-blend-darken" src={imgF} alt="ProfilePicture" />
-				</div>
-			</div>
-			<span className="flex justify-center pt-2 text-xl font-medium text-white">
-				Placeholder
-			</span>
-			<LinksList />
-			<LogoutButton />
-		</nav>
-	);
+  const { userData } = useSocketContext();
+  return (
+    <nav className="w-[246px] shrink-0 h-full bg-color2 border border-none rounded-xl overflow-hidden flex flex-col">
+      <div className="flex items-center justify-center w-full bg-color1 rounded-tr-xl border border-none rounded-tl-xl h-[75px]">
+        <div className="p-1 py-2 border border-white rounded-full ">
+          <img className="mix-blend-darken" src={imgF} alt="ProfilePicture" />
+        </div>
+      </div>
+      <span className="flex justify-center pt-2 text-xl font-medium text-white">
+        {userData?.username}
+      </span>
+      <LinksList />
+      <LogoutButton />
+    </nav>
+  );
 };
 
 export default NavBar;
