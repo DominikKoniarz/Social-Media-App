f332b9ae8428991aea4ca89c1dcb2df04ced1fbf
Vocyl <platyna155@gmail.com>
2023-12-31 20:05:40 +0100

    UserAvatar delete button

--- frontend/src/Pages/ProfilePage/ProfileEdit/AvatarImage.tsx
+++ frontend/src/Pages/ProfilePage/ProfileEdit/AvatarImage.tsx
@@ -1,39 +1,60 @@
-import { FaPen, FaRegUser } from "react-icons/fa6";
+import { FaPen, FaTrashCan, FaRegUser } from "react-icons/fa6";
 import ProfilePicture from "@assets/images/ProfilePicture.png";
 import useSocketContext from "hooks/useSocketContext";
 import { APP_URL } from "constraints";
+import { useState } from "react";
+import AvatarDeleteModal from "./AvatarDeleteModal";
 
 type Props = {
-	setProfileImageModal: React.Dispatch<React.SetStateAction<boolean>>;
+  setProfileImageModal: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 const AvatarImage = ({ setProfileImageModal }: Props) => {
-	const { userData } = useSocketContext();
+  const [deleteImageModalOpen, setDeleteImageModalOpen] =
+    useState<boolean>(false);
+  const { userData } = useSocketContext();
 
-	const imageSrc = userData
-		? `${APP_URL}/media/${userData.id}/avatar/${userData.avatarImage}`
-		: ProfilePicture;
+  const imageSrc = userData
+    ? `${APP_URL}/media/${userData.id}/avatar/${userData.avatarImage}`
+    : ProfilePicture;
 
-	return (
-		<button
-			className="absolute bottom-0 block translate-y-1/2 rounded-md left-10"
-			onClick={() => setProfileImageModal(true)}
-		>
-			<div className="relative grid bg-slate-50 place-items-center w-[128px] h-[118px] rounded-md border-slate-200 border-2 group overflow-hidden">
-				<div className="absolute top-0 left-0 z-10 grid w-full h-full text-xl text-white transition-all duration-300 bg-black bg-opacity-0 rounded-md opacity-0 place-items-center group-hover:opacity-100 group-hover:bg-opacity-50">
-					<FaPen />
-				</div>
-				{userData && userData.avatarImage ? (
-					<img
-						className="absolute object-cover w-full h-full -translate-x-1/2 -translate-y-1/2 rounded-md left-1/2 top-1/2"
-						src={imageSrc}
-						alt="ProfilePicture"
-					/>
-				) : (
-					<FaRegUser className="text-3xl" />
-				)}
-			</div>
-		</button>
-	);
+  return (
+    <div className="absolute bottom-0 block translate-y-1/2 left-10 w-[128px] h-[118px] rounded-md border-slate-200 border-2 overflow-hidden group">
+      {userData && userData.avatarImage ? (
+        <img
+          className="absolute z-10 object-cover w-full h-full"
+          src={imageSrc}
+          alt="ProfilePicture"
+        />
+      ) : (
+        <FaRegUser className="text-3xl" />
+      )}
+      <div className="relative z-20 w-full h-full transition-all duration-300 bg-black bg-opacity-0 opacity-0 group-hover:opacity-100 group-hover:bg-opacity-50">
+        <div className="absolute flex items-center justify-center w-full h-full gap-4 text-2xl">
+          <button
+            className="z-20 text-white w-fit h-fit"
+            onClick={() => setProfileImageModal(true)}
+          >
+            <FaPen />
+          </button>
+          {userData && userData.avatarImage && (
+            <>
+              <button
+                className="z-20 text-red-500 w-fit h-fit"
+                onClick={() => setDeleteImageModalOpen(true)}
+              >
+                <FaTrashCan />
+              </button>
+
+              <AvatarDeleteModal
+                setDeleteImageModalOpen={setDeleteImageModalOpen}
+                deleteImageModalOpen={deleteImageModalOpen}
+              />
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 export default AvatarImage;
