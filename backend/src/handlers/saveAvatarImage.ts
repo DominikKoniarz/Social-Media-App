d515cd51c527c62d6bc054843107d4f8f867192b
Vocyl <platyna155@gmail.com>
2023-12-31 19:10:50 +0100

    Done delete avatar image handler

--- backend/src/handlers/saveAvatarImage.ts
+++ backend/src/handlers/saveAvatarImage.ts
@@ -1,10 +1,10 @@
 import { Socket } from "socket.io";
 import {
-	ClientToServerEvents,
-	CropData,
-	InterServerEvents,
-	ServerToClientEvents,
-	SocketData,
+  ClientToServerEvents,
+  CropData,
+  InterServerEvents,
+  ServerToClientEvents,
+  SocketData,
 } from "../../../types/socket.io";
 import getDbInstance from "../initializers/db";
 import { logError } from "../middleware/errorHandler";
@@ -18,107 +18,107 @@ const prisma = getDbInstance();
 
 const getUserDir = (userId: string): string => path.join(MEDIA_DIR, userId);
 const getUserAvatarDir = (userId: string): string =>
-	path.join(MEDIA_DIR, userId, "avatar");
+  path.join(MEDIA_DIR, userId, "avatar");
 
 const createRequiredDirs = async (userId: string) => {
-	const userDir = getUserDir(userId);
-	await createDirIfNotExists(userDir);
+  const userDir = getUserDir(userId);
+  await createDirIfNotExists(userDir);
 
-	const avatarDir = getUserAvatarDir(userId);
-	await createDirIfNotExists(avatarDir);
+  const avatarDir = getUserAvatarDir(userId);
+  await createDirIfNotExists(avatarDir);
 };
 
 const validateCropData = (cropData: CropData): boolean => {
-	const { height, width, x, y, scaleX, scaleY } = cropData;
+  const { height, width, x, y, scaleX, scaleY } = cropData;
 
-	if (height < 0 || width < 0 || x < 0 || y < 0 || scaleX < 0 || scaleY < 0)
-		return false;
+  if (height < 0 || width < 0 || x < 0 || y < 0 || scaleX < 0 || scaleY < 0)
+    return false;
 
-	return true;
+  return true;
 };
 
 const saveAvatarImage = (
-	socket: Socket<
-		ClientToServerEvents,
-		ServerToClientEvents,
-		InterServerEvents,
-		SocketData
-	>
+  socket: Socket<
+    ClientToServerEvents,
+    ServerToClientEvents,
+    InterServerEvents,
+    SocketData
+  >
 ) => {
-	socket.on(
-		"saveAvatarImage",
-		async (imageBuffer, imageName, cropData, sendImageSrc) => {
-			const userId = socket.data.userId;
-			// shouldn't happen bc of the middleware which always set the userId
-			if (!userId) return socket.disconnect();
+  socket.on(
+    "saveAvatarImage",
+    async (imageBuffer, imageName, cropData, sendImageSrc) => {
+      const userId = socket.data.userId;
+      // shouldn't happen bc of the middleware which always set the userId
+      if (!userId) return socket.disconnect();
 
-			try {
-				const foundUser = await prisma.user.findUnique({
-					where: {
-						id: userId,
-					},
-				});
+      try {
+        const foundUser = await prisma.user.findUnique({
+          where: {
+            id: userId,
+          },
+        });
 
-				if (!foundUser) return socket.disconnect();
+        if (!foundUser) return socket.disconnect();
 
-				const areCropDataValid = validateCropData(cropData);
-				if (!areCropDataValid) throw new Error("Invalid crop data!");
+        const areCropDataValid = validateCropData(cropData);
+        if (!areCropDataValid) throw new Error("Invalid crop data!");
 
-				if (foundUser.avatarImage)
-					await fsp.rm(
-						path.join(getUserAvatarDir(userId), foundUser.avatarImage),
-						{ force: true }
-					);
+        if (foundUser.avatarImage)
+          await fsp.rm(
+            path.join(getUserAvatarDir(userId), foundUser.avatarImage),
+            { force: true }
+          );
 
-				await createRequiredDirs(userId);
+        await createRequiredDirs(userId);
 
-				const extractData: sharp.Region = {
-					height: parseInt(cropData.height.toString()),
-					width: parseInt(cropData.width.toString()),
-					left: parseInt(cropData.x.toString()),
-					top: parseInt(cropData.y.toString()),
-				};
+        const extractData: sharp.Region = {
+          height: parseInt(cropData.height.toString()),
+          width: parseInt(cropData.width.toString()),
+          left: parseInt(cropData.x.toString()),
+          top: parseInt(cropData.y.toString()),
+        };
 
-				const croppedImageBuffer = await sharp(imageBuffer)
-					.extract(extractData)
-					.resize({ height: 128, width: 128 })
-					.webp()
-					.toBuffer();
+        const croppedImageBuffer = await sharp(imageBuffer)
+          .extract(extractData)
+          .resize({ height: 128, width: 128 })
+          .webp()
+          .toBuffer();
 
-				const imageNameWithoutExtension = path.parse(imageName).name;
-				const newImageName = `${imageNameWithoutExtension}.webp`;
+        const imageNameWithoutExtension = path.parse(imageName).name;
+        const newImageName = `${imageNameWithoutExtension}.webp`;
 
-				await fsp.writeFile(
-					path.join(getUserAvatarDir(userId), newImageName),
-					croppedImageBuffer
-				);
+        await fsp.writeFile(
+          path.join(getUserAvatarDir(userId), newImageName),
+          croppedImageBuffer
+        );
 
-				await prisma.user.update({
-					data: {
-						avatarImage: newImageName,
-					},
-					where: {
-						id: userId,
-					},
-				});
+        await prisma.user.update({
+          data: {
+            avatarImage: newImageName,
+          },
+          where: {
+            id: userId,
+          },
+        });
 
-				sendImageSrc(null, newImageName);
-			} catch (error) {
-				const errorMessage =
-					error instanceof Error ? error.message : "Unknown server error!";
+        sendImageSrc(null, newImageName);
+      } catch (error) {
+        const errorMessage =
+          error instanceof Error ? error.message : "Unknown server error!";
 
-				sendImageSrc(errorMessage, null);
+        sendImageSrc(errorMessage, null);
 
-				socket.emit("serverError", errorMessage);
+        socket.emit("serverError", errorMessage);
 
-				logError(
-					`Get user data error! Socket id: ${socket.id}`,
-					errorMessage,
-					"socketErrorsLog.txt"
-				);
-			}
-		}
-	);
+        logError(
+          `Save user avatar image error! Socket id: ${socket.id}`,
+          errorMessage,
+          "socketErrorsLog.txt"
+        );
+      }
+    }
+  );
 };
 
 export default saveAvatarImage;
