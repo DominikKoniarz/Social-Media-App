3403966ace9b12c58e04385cb40d404dc6da8693
Vocyl <platyna155@gmail.com>
2024-01-03 19:57:14 +0100

    Added addpost component

--- frontend/src/Pages/RootPage/RootPage.tsx
+++ frontend/src/Pages/RootPage/RootPage.tsx
@@ -1,10 +1,15 @@
 import Suggestions from "@components/Suggestions";
 import Post from "./Post";
+import { useState } from "react";
+import AddPost from "./AddPost";
 
 const RootPage = () => {
+  const [newPost, setNewPost] = useState<string>("");
+
   return (
     <div className="flex h-full gap-4 px-1 md:gap-16 md:px-32 w-fit">
       <ul className="h-full mx-auto space-y-4 overflow-y-auto posts w-fit">
+        <AddPost newPost={newPost} setNewPost={setNewPost} />
         <Post />
         <Post />
         <Post />
