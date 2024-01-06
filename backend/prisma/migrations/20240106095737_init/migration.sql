7a21ef2c05bcd1b6bf36a29df74dfdce372700f5
DominikKoniarz <dominik.koniarz.development@gmail.com>
2024-01-06 10:57:57 +0100

    Added Post model

--- backend/prisma/migrations/20240106095737_init/migration.sql
+++ backend/prisma/migrations/20240106095737_init/migration.sql
@@ -31,5 +31,19 @@ CREATE TABLE `RefreshToken` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` VARCHAR(191) NOT NULL,
+    `textContent` VARCHAR(2048) NOT NULL,
+    `image` VARCHAR(256) NULL,
+    `publishedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `userId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `RefreshToken` ADD CONSTRAINT `RefreshToken_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
