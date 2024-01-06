17e5442d3e4bbe7260805653e2ebede746e721e2
DominikKoniarz <dominik.koniarz.development@gmail.com>
2024-01-06 18:58:23 +0100

    Done getCurrentUserPosts

---
+++ backend/src/handlers/getCurrentUserPosts.ts
@@ -0,0 +1,71 @@
+import { Socket } from "socket.io";
+import {
+	ClientToServerEvents,
+	InterServerEvents,
+	ServerToClientEvents,
+	SocketData,
+} from "../../../types/socket.io";
+import getDbInstance from "../initializers/db";
+import { logError } from "../middleware/errorHandler";
+
+const prisma = getDbInstance();
+
+const getCurrentUserPosts = (
+	socket: Socket<
+		ClientToServerEvents,
+		ServerToClientEvents,
+		InterServerEvents,
+		SocketData
+	>
+) => {
+	socket.on("getCurrentUserPosts", async (offset, sendPosts) => {
+		const userId = socket.data.userId;
+
+		// shouldn't happen bc of the middleware which always set the userId
+		if (!userId) return socket.disconnect();
+
+		try {
+			const foundUser = await prisma.user.findUnique({
+				where: {
+					id: userId,
+				},
+				select: {
+					id: true,
+				},
+			});
+
+			if (!foundUser) return socket.disconnect();
+
+			if (offset < 0) throw new Error("Offset must be greater or equal 0!");
+
+			const posts = await prisma.post.findMany({
+				where: {
+					userId: userId,
+				},
+				select: {
+					id: true,
+					textContent: true,
+					publishedAt: true,
+					image: true,
+				},
+				skip: offset,
+				take: 7,
+			});
+
+			sendPosts(null, posts);
+		} catch (error) {
+			const errorMessage =
+				error instanceof Error ? error.message : "Unknown server error!";
+
+			sendPosts(errorMessage, null);
+
+			logError(
+				`Get user data error! Socket id: ${socket.id}`,
+				error instanceof Error ? error.message : "unknown error",
+				"socketErrorsLog.txt"
+			);
+		}
+	});
+};
+
+export default getCurrentUserPosts;
