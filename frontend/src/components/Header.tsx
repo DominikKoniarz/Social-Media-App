1a58d28c07e3e40219a9431faf73096fecd58802
DominikKoniarz <dominik.koniarz.development@gmail.com>
2024-01-08 15:37:47 +0100

    Deleted notification button and added info when user has no posts

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,31 +1,30 @@
 import LogoText from "./LogoText";
 import Form from "./Form";
-import NotificationIcon from "./NotificationIcon";
 import imgF from "@assets/images/imgF.png";
 import { useState } from "react";
 import AddPostButton from "@components/AddPostButton";
 import AddPostModal from "./AddPostModal";
 
 const Header = () => {
-  const [addPostModalOpen, setAddPostModalOpen] = useState<boolean>(false);
-  return (
-    <header className="w-full h-[74px] bg-white">
-      <div className="py-2 px-4 mx-auto max-w-[1920px] w-full flex items-center h-full">
-        <div className="flex">
-          <img className="mix-blend-darken" src={imgF} alt="Img F" />
-          <LogoText />
-        </div>
-        <Form />
-        <div className="flex items-center gap-4">
-          <AddPostButton setAddPostModalOpen={setAddPostModalOpen} />
-          <NotificationIcon />
-        </div>
-      </div>
-      <AddPostModal
-        addPostModalOpen={addPostModalOpen}
-        setAddPostModalOpen={setAddPostModalOpen}
-      />
-    </header>
-  );
+	const [addPostModalOpen, setAddPostModalOpen] = useState<boolean>(false);
+
+	return (
+		<header className="w-full h-[74px] bg-white">
+			<div className="py-2 px-4 mx-auto max-w-[1920px] w-full flex items-center h-full">
+				<div className="flex">
+					<img className="mix-blend-darken" src={imgF} alt="Img F" />
+					<LogoText />
+				</div>
+				<Form />
+				<div className="flex items-center gap-4">
+					<AddPostButton setAddPostModalOpen={setAddPostModalOpen} />
+				</div>
+			</div>
+			<AddPostModal
+				addPostModalOpen={addPostModalOpen}
+				setAddPostModalOpen={setAddPostModalOpen}
+			/>
+		</header>
+	);
 };
 export default Header;
