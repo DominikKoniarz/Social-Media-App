b3030a0670a3235756f86f133847ec36a4a8b40f
Vocyl <platyna155@gmail.com>
2024-01-10 13:33:30 +0100

    More responsivenes

--- frontend/src/components/AddPostButton.tsx
+++ frontend/src/components/AddPostButton.tsx
@@ -1,3 +1,5 @@
+import { FaRegSquarePlus } from "react-icons/fa6";
+
 type Props = {
   setAddPostModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
@@ -7,9 +9,12 @@ const AddPostButton = ({ setAddPostModalOpen }: Props) => {
     <button
       onClick={() => setAddPostModalOpen(true)}
       type="submit"
-      className="absolute px-4 py-2 text-white bg-teal-500 rounded-md shadow right-4 w-fit"
+      className="absolute px-3 py-2 text-white bg-teal-500 rounded-md shadow md:px-4 md:py-2 right-4 w-fit"
     >
-      Publish Post
+      <p className="hidden md:block">Publish Post</p>
+      <p className="items-center justify-center block text-3xl md:hidden">
+        <FaRegSquarePlus />
+      </p>
     </button>
   );
 };
