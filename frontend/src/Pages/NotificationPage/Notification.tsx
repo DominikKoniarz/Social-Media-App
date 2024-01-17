7d71750425b67dfa97a6b433008d02f6b9c8436b
Vocyl <platyna155@gmail.com>
2024-01-17 19:49:52 +0100

    Fixed responsivenes in notification page

--- frontend/src/Pages/NotificationPage/Notification.tsx
+++ frontend/src/Pages/NotificationPage/Notification.tsx
@@ -1,22 +1,23 @@
 const Notification = () => {
   return (
-    <li className="flex items-center gap-2 p-8 bg-white border-l-4 border-teal-500 rounded-md">
+    <li className="flex items-center gap-2 p-1 bg-white border-l-4 border-teal-500 rounded-md md:px-4 2xl:p-8">
       <div className="items-center justify-center p-6 border-2 border-black rounded-full"></div>
       <div className="flex flex-col">
         <div className="flex items-center">
-          <p className="ml-2 text-lg font-medium text-zinc-950 font-family1">
+          <p className="ml-2 text-sm font-medium md:text-base 2xl:text-lg text-zinc-950 font-family1">
             Placeholder Name
           </p>
-          <p className="ml-1 text-base font-light lowercase text-slate-800 font-family1">
+          <p className="ml-1 text-xs font-light lowercase md:text-sm 2xl:text-base text-slate-800 font-family1">
             @AliasDanegoUżytkownika
           </p>
-          <p className="text-base font-light lowercase text-slate-800 font-family1">
+          <p className="text-xs font-light lowercase md:text-sm 2xl:text-base text-slate-800 font-family1">
             - 10min
           </p>
         </div>
-        <p className="text-black ml-2 text-[15px] font-light font-family1 capitalize leading-[23px]">
+        <p className="text-black ml-2 text-sm  max-h-32 overflow-hidden 2xl:text-base font-light font-family1 capitalize leading-[23px]">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text
+          Lorem Ipsum has been the industry's standard dummy text
         </p>
       </div>
     </li>
