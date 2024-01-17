e75cf8bc77df181b905d46490371cb495dcdb1d7
Vocyl <platyna155@gmail.com>
2024-01-17 19:33:52 +0100

    Fixed responsivenes

--- frontend/src/Pages/ProfilePage/ProfilePosts.tsx
+++ frontend/src/Pages/ProfilePage/ProfilePosts.tsx
@@ -5,41 +5,45 @@ import UserPost from "@components/UserPost";
 import useSocketContext from "hooks/useSocketContext";
 
 const ProfilePosts = () => {
-	const { isLoading, error, posts } = useGetCurrentUserPosts();
-	const { userData } = useSocketContext();
+  const { isLoading, error, posts } = useGetCurrentUserPosts();
+  const { userData } = useSocketContext();
 
-	useEffect(() => {
-		if (error && import.meta.env.DEV) console.log(error);
-	}, [error]);
+  useEffect(() => {
+    if (error && import.meta.env.DEV) console.log(error);
+  }, [error]);
 
-	return (
-		<>
-			{isLoading && (
-				<div className="flex justify-center w-full p-4">
-					<SpinnerLoader />
-				</div>
-			)}
-			{!isLoading && error && (
-				<div className="w-full p-4 text-center">Wystąpił błąd!</div>
-			)}
-			{!isLoading && !error && posts && posts.length === 0 && (
-				<div className="w-full p-4 text-center">Brak wpisów!</div>
-			)}
-			{!isLoading && !error && posts && userData && (
-				<ul className="w-full space-y-4 h-fit">
-					{posts.map((post) => (
-						<UserPost
-							userData={userData}
-							key={post.id}
-							id={post.id}
-							textContent={post.textContent}
-							publishedAt={new Date(post.publishedAt)}
-							image={post.image}
-						/>
-					))}
-				</ul>
-			)}
-		</>
-	);
+  return (
+    <>
+      {isLoading && (
+        <div className="flex justify-center w-full p-4">
+          <SpinnerLoader />
+        </div>
+      )}
+      {!isLoading && error && (
+        <div className="w-full p-4 text-center">Wystąpił błąd!</div>
+      )}
+      {!isLoading && !error && posts && posts.length === 0 && (
+        <div className="flex justify-center w-full p-4">Brak wpisów!</div>
+      )}
+      {!isLoading && !error && posts && userData && (
+        <ul
+          className={`w-full space-y-4 h-fit ${
+            posts.length === 0 ? "hidden" : "block"
+          }`}
+        >
+          {posts.map((post) => (
+            <UserPost
+              userData={userData}
+              key={post.id}
+              id={post.id}
+              textContent={post.textContent}
+              publishedAt={new Date(post.publishedAt)}
+              image={post.image}
+            />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 };
 export default ProfilePosts;
