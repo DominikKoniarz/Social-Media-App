1a5d9c69a1a5310cc85b4ca4532b371d4b938724
DominikKoniarz <dominik.koniarz.development@gmail.com>
2024-01-28 13:23:08 +0100

    setRefreshTokenCookie modifications

--- backend/src/lib/cookies.ts
+++ backend/src/lib/cookies.ts
@@ -5,14 +5,15 @@ export const REFRESH_TOKEN_COOKIE_NAME =
 	process.env.REFRESH_TOKEN_COOKIE_NAME || "appRefreshToken";
 
 export const setRefreshTokenCookie = (res: Response, token: string) => {
+	const cookieDomain =
+		process.env.NODE_ENV === "production"
+			? process.env.COOKIE_DOMAIN
+			: "localhost";
+
 	res.cookie(REFRESH_TOKEN_COOKIE_NAME, token, {
 		httpOnly: true,
 		secure: process.env.NODE_ENV === "production" ? true : false,
-		path: "/",
-		domain:
-			process.env.NODE_ENV === "production"
-				? process.env.COOKIE_DOMAIN
-				: "localhost",
+		domain: cookieDomain,
 		maxAge: REFRESH_TOKEN_MAX_AGE_MS,
 	});
 };
